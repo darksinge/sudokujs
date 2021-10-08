664f430f017924272ee412c1c89f31a28f24862f
Craig Blackburn <cr.blackburn89@gmail.com>
2021-10-08 13:44:35 -0600

    Add lone-single strat

---
+++ test/lone-single.test.js
@@ -0,0 +1,23 @@
+const assert = require('assert')
+const { Puzzle } = require('../lib/puzzle')
+const { loneSingle } = require('../lib/strategies')
+
+describe('Long Single Strategy', () => {
+  it('should be enough to solve a simple puzzle', () => {
+    const p = Puzzle.fromSymbols([
+      [1, 0, 0, 0],
+      [0, 2, 0, 0],
+      [0, 3, 4, 0],
+      [0, 0, 0, 3]
+    ])
+
+    assert.equal(p.isSolved(), false)
+
+    while (p.applyStrategy(loneSingle)) {
+      /** noop */
+    }
+
+    assert.equal(p.isSolved(), true)
+    p.print()
+  })
+})
